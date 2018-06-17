3fc1aff2193a2325145e4543e304a6ea9dea922a
muturgan <caxap_8787@mail.ru>
2018-06-18 01:15:59 +0300

    server was divided into 3 separate files

---
+++ server/controller.js
@@ -0,0 +1,85 @@
+const sqlRequest = require('./db.js');
+
+const Base64 = require('js-base64').Base64;
+
+const controller = {
+  initSignIn: (data) => {
+    return sqlRequest(`SELECT password FROM users WHERE name="${ Base64.encode(data.name) }";`)
+      .then((rows) => {
+        if ((rows[0] === undefined) || (Base64.decode(rows[0].password) !== data.password)) {
+          return new Error('signInError');
+        } else {
+          return sqlRequest(`UPDATE users SET status = 'online' WHERE name="${ Base64.encode(data.name) }";`)
+          .then(() => {
+            return sqlRequest(`SELECT id, name, status FROM users WHERE name="${ Base64.encode(data.name) }"`);
+          }).then((rows) => {
+            rows[0].name = Base64.decode(rows[0].name);
+            return rows[0];
+          });
+        }
+      }).catch((error) => {
+        return error;
+      });
+  },
+  
+  initSignUp: (data) => {
+    return sqlRequest(`SELECT name FROM users WHERE name="${ Base64.encode(data.name) }";`)
+      .then((rows) => {
+        if (rows[0]) {
+          return new Error('signUpError');
+        } else {
+          return sqlRequest(`INSERT INTO users (name, password, status) VALUES ('${ Base64.encode(data.name) }', '${ Base64.encode(data.password) }', 'online');`)
+          .then(() => {
+            return sqlRequest(`SELECT id, name, status FROM users WHERE name="${ Base64.encode(data.name) }"`);
+          }).then((rows) => {
+            return rows[0];
+          });
+        }
+      }).catch((error) => {
+        return error;
+      });
+  },
+  
+  initChat: () => {
+    return sqlRequest(`SELECT *, NULL AS password FROM users, messages WHERE messages.author_id = users.id AND messages.room="public";`)
+    .then((rows) => {
+      for (let row of rows) {
+        row.name = Base64.decode(row.name);
+        row.text = Base64.decode(row.text);
+      };
+      
+      rows.sort((row1, row2) => {
+        if (row1.date > row2.date) return 1;
+        if (row1.date < row2.date) return -1;
+      });
+      
+      return rows;
+    }).catch((error) => {
+        return error;
+    });
+  },
+  
+  messageFromClient: (message) => {
+    const now = Date.now();
+    
+    return sqlRequest(`INSERT INTO messages (date, text, author_id) VALUES (${now},'${Base64.encode(message.text)}', '${message.author_id}');`)
+    .then((rows) => {
+      return sqlRequest(`SELECT *, NULL AS password FROM users, messages WHERE users.id = ${message.author_id} AND messages.room="public" AND messages.date=${now} AND messages.text="${Base64.encode(message.text)}";`)
+    }).then((rows) => {
+      rows[0].name = Base64.decode(rows[0].name);
+      rows[0].text = Base64.decode(rows[0].text);
+      return rows[0];
+    }).catch((error) => {
+      return error;
+    }); 
+  },
+  
+  userLeave: (user) => {
+    return sqlRequest(`UPDATE users SET status = 'offline' WHERE id="${ user.id }";`)
+    .catch((error) => {
+      return error;
+    });
+  },
+};
+
+module.exports = controller;
---
+++ server/db.js
@@ -0,0 +1,22 @@
+const mysql = require('promise-mysql');
+
+const mysqlConfig = {
+  host: '128.199.39.117',
+  user: 'node',
+  password: 'edon',
+  database: 'pretty_chat',
+  port: 3306,
+};
+
+const sqlRequest = (req) => {
+  return mysql.createConnection(mysqlConfig)
+    .then((connection) => {
+      let rows = connection.query(req);
+      connection.end();
+      return rows;
+    }).catch((error) => {
+      return error;
+    });
+};
+
+module.exports = sqlRequest;
